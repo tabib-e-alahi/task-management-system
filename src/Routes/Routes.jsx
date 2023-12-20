0451f38166964a653a795900aa2b3f12631d4069
tabib-e-alahi <etabib.alahi@gmail.com>
2023-12-21 02:36:13 +0600

    MUI installed and login,signup routes added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
 
 export const router = createBrowserRouter([
     {
@@ -10,6 +12,14 @@ export const router = createBrowserRouter([
         {
             path: "/",
             element:<Home></Home>
+        },
+        {
+            path: "/login",
+            element:<Login></Login>
+        },
+        {
+            path: "/register",
+            element:<Register></Register>
         }
       ]
     },
